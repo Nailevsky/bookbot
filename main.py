da7bb8a8c71571ae5551fcf5c5f4ee44ac9dde5a
Nailevsky <nailevsky@yahoo.co.nz>
2025-04-30 17:35:47 -0400

    Last

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+from stats import get_num_words, get_char_count, sort_char_counts
+
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        return f.read()
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    book_text = get_book_text(path)
+    num_words = get_num_words(book_text)
+    char_counts = get_char_count(book_text)
+    sorted_chars = sort_char_counts(char_counts)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for entry in sorted_chars:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
+
+main()
